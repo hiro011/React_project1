4f76f05e09e3140984613f740a92a9223b4de93d
Ahmed Mehamedyesuf <mehamedseid002@gmail.com>
2023-02-12 11:20:29 +0300

    third commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use(logger);
+
+router.get("/", (req, res) => {
+  console.log( req.query.name)
+  res.send("User List");
 });
+
+router.get("/new", (req, res) => {
+  res.render("users/new");
+});
+
+router.post("/", (req, res) => {
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+  res.send("Creat New User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`Get user with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with ID ${req.params.id}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  console.log("Here")
-  res.render("home")
-})
+app.set("view engine", "ejs");
 
-app.listen(3000)
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+
+app.listen(3000);
